2f4996f7f26621cc6875ab2e274a2916759cb3b9
sameghosts <seanfeiner.brown@gmail.com>
2021-07-06 23:40:49 -0400

    added route

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request
-
+from flask import json
 #add rebeccas dependencies
 #add a dummy json object
 #try dummy import using posting
@@ -11,8 +11,13 @@ app = Flask(__name__)
 # Add app.route methods = POST
 @app.route("/")
 def hello():
-  return "Hello World"
-  
+  return "Hello World from the Flask TRKR Service API"
+
+#route for service
+@app.route("/runNLP")
+def nlp():
+  return "Hello from NLP"
+
 #app .run
 if __name__ == "__main__":
   app.run()
